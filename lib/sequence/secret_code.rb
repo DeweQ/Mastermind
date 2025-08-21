8cff3114606f17653132b90310b4b39c1f2fda24
DeweQ <thedeweq@yandex.ru>
2025-08-21 13:55:44 +0300

    Add SecretCode class with validation

---
+++ lib/sequence/secret_code.rb
@@ -0,0 +1,14 @@
+module Mastermind
+  # Class containing a secrete code. Validates sequence on initializing
+  class SecretCode < Sequence
+    def initialize(colors)
+      raise ArgumentError, "Colors in secret code must be unique" unless valid?(colors)
+
+      super
+    end
+
+    def valid?(colors)
+      colors.uniq.size == colors.size
+    end
+  end
+end
