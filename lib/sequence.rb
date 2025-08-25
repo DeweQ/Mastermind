5265f4eeadc22ce5e380a639f3bc32b9e5540291
DeweQ <thedeweq@yandex.ru>
2025-08-25 13:55:57 +0300

    Add Enumerable mixin to Sequence

--- lib/sequence.rb
+++ lib/sequence.rb
@@ -3,8 +3,15 @@ require "colorize"
 module Mastermind
   # Contains a sequence of colors.
   class Sequence
+    include Enumerable
+
+    def self.colors
+      %i[red green blue yellow magenta cyan grey white]
+    end
+
     def initialize(colors = %i[red green blue yellow])
       @colors = colors
+      @colors.freeze
       @display = ""
     end
 
@@ -14,7 +21,16 @@ module Mastermind
       @colors.each do |c|
         @display << "⬮".colorize(c)
       end
+      @display = @display.colorize(background: :black)
       @display
+    end
+
+    def each(&block)
+      @colors.each(&block)
+    end
+
+    def [](index)
+      @colors[index]
     end
 
     def to_s
