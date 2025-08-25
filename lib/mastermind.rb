9df20a0b9ae3a8134fb26cda2f3284d881e60a99
DeweQ <thedeweq@yandex.ru>
2025-08-25 14:00:27 +0300

    Add Mastermind superclass

---
+++ lib/mastermind.rb
@@ -0,0 +1,12 @@
+module Mastermind
+  # Mastermind parent class. Generating secret code and feedback for player's guess.
+  class Mastermind
+    def generate_code
+      %i[blue yellow red green]
+    end
+
+    def feedback(guess)
+      # TODO: fill method
+    end
+  end
+end
