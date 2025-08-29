7a7bad8cf548fb3abb8a1084044dd4924eb6e0cb
DeweQ <thedeweq@yandex.ru>
2025-08-29 13:41:54 +0300

    Add output of game progress

--- lib/game.rb
+++ lib/game.rb
@@ -7,27 +7,56 @@ require_relative "sequence/secret_code"
 module Mastermind
   # Game master class.
   class Game
+    attr_reader :winner, :code
+
     def initialize(mastermind, guesser, difficulty: 6)
       @mastermind = mastermind
       @guesser = guesser
       @mastermind.difficulty = difficulty
       @guesser.difficulty = difficulty
+      @history = []
+      @finished = false
+      @code = @mastermind.generate_code
     end
 
-    def play_round
-      guess = @guesser.make_guess
-      puts guess.display
-      @mastermind.give_feedback(guess)
+    def finished?
+      @finished
     end
 
     def play
-      code = @mastermind.generate_code
       rounds = 0
+      feedback = ""
       loop do
-        feedback = play_round
         rounds += 1
-        puts feedback.to_seq.display
+        feedback = play_round
         break if feedback.correct? || rounds >= 12
+      end
+      @finished = true
+      @winner = feedback.correct? ? "Guesser" : "Mastermind"
+      finalize
+    end
+
+    private
+
+    def play_round
+      update_display
+      guess = @guesser.make_guess
+      feedback = @mastermind.give_feedback(guess)
+      @history << [guess, feedback]
+      feedback
+    end
+
+    def finalize
+      update_display
+      puts "Winner is #{@winner}" if finished?
+      puts "Secret code is #{@code.display}" if @winner == "Mastermind"
+    end
+
+    def update_display
+      system("clear") || system("cls")
+      puts "*** MASTERMIND ***"
+      @history.each do |entry|
+        puts "#{entry[0].display}  :  #{entry[1].to_seq.display}"
       end
     end
   end
