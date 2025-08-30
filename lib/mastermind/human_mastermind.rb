e32c0167fd4f319816873ad15c66032aedf2f535
DeweQ <thedeweq@yandex.ru>
2025-08-30 10:37:20 +0300

    Add HumanMastermind class

---
+++ lib/mastermind/human_mastermind.rb
@@ -0,0 +1,34 @@
+require_relative "../mastermind"
+require_relative "../sequence"
+require_relative "../sequence/secret_code"
+require_relative "../sequence/feedback"
+
+module Mastermind
+  # Human player as Mastermind
+  class HumanMastermind < Mastermind
+    def generate_code
+      loop do
+        puts "Create a code consisting of 4 distinct colors."
+        puts "Available colors: #{Sequence.colors.first(difficulty).join ', '}"
+        input = gets.chomp.split.map(&:to_sym)
+        # TODO: Clean this mess
+        break if validate_input(input)
+
+        puts "Secret code must be a sequence of 4 distinct colors from available color list"
+      end
+    end
+
+    def give_feedback(guess)
+      # TODO: Give ability for the player to give feedback themselves
+      Feedback.generate_feedback(code, guess)
+    end
+
+    private
+
+    def validate_input(colors)
+      validity = SecretCode.valid?(colors)
+      self.code = SecretCode.new(colors) if validity
+      validity
+    end
+  end
+end
--- lib/sequence/secret_code.rb
+++ lib/sequence/secret_code.rb
@@ -4,13 +4,18 @@ module Mastermind
   # Class containing a secrete code. Validates sequence on initializing
   class SecretCode < Sequence
     def initialize(colors)
-      raise ArgumentError, "Colors in secret code must be unique" unless valid?(colors)
+      unless SecretCode.valid?(colors)
+        raise ArgumentError,
+              "Secret code must be a sequence of 4 unique colors from the list of available colors"
+      end
 
       super
     end
 
-    def valid?(colors)
-      colors.uniq.size == colors.size
+    def self.valid?(colors)
+      colors.uniq.size == colors.size &&
+        colors.size == 4 &&
+        colors.all? { |color| Sequence.colors.include? color }
     end
   end
 end
